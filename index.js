d698c0f7285dbc4a7417ecd64b4097ba4a8ab287
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-07-27 13:12:11 +0300

    complete iteration 5-7

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ class SortedList {
     if (this.items.length === 0) {
       throw new Error("EmptySortedList");
     } else {
-      return this.items.reduce((a, b) => Math.max(a, b), 0);
+      // return this.items.reduce((a, b) => Math.max(a, b), 0);
+      return Math.max(...this.items);
     }
   }
 
@@ -31,13 +32,21 @@ class SortedList {
     if (this.items.length === 0) {
       throw new Error("EmptySortedList");
     } else {
-      return Math.min(this.items);
+      return Math.min(...this.items);
     }
   }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((accum, currentValue) => accum + currentValue, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
